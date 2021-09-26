48190fd380eb119358b26e2d3fca9c87133c8c10
serg <j0nybr4v0@gmail.com>
2021-09-26 18:27:12 +0300

    add webpack-analyze & http-server for prod serve for now; fix bug; resize imgs; build\start scripts

--- src/app/cards/card/card.component.ts
+++ src/app/cards/card/card.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  @Input() image: string;
+  @Input() image: any;
 
   constructor() { }
 
