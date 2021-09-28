38d941757359a5644883436e7c42f6208dc1e9e0
serg <j0nybr4v0@gmail.com>
2021-09-28 13:34:54 +0300

    proxy to indexhtml for ng router

--- readfiles.js
+++ readfiles.js
@@ -6,6 +6,6 @@ const name = path.split('/').pop();
 console.log('read dir: ' + path, ' person: ' + name);
 const files = fs.readdir(path, function (err, dirFiles) {
   // console.log(dirFiles);
-  const output = dirFiles.map(f => `{path: 'assets/images/${name}/${f}', name: '${f}', metas: {description: 'ljhsdfhjljhdsf', year: 0}},\n`).join('');
+  const output = dirFiles.map(f => `{path: 'assets/images/${name}/${f}', name: '${f}', metas: {albums: [0, 1], description: 'ljhsdfhjljhdsf', year: 0}},\n`).join('');
   fs.writeFileSync('new_file_names_json.txt', output, 'utf8');
 });
